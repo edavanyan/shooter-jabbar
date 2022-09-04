3bf6b8f8f41be3823bd70a4035401b5ae9b723ae
edgar <edgaravanyan@gmail.com>
2022-09-04 23:23:54 +0400

    deleted commments

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -33,12 +33,6 @@ public class PlayerController : MonoBehaviour
         {
             gameObject.transform.forward = move;
         }
-        //
-        // // Changes the height position of the player..
-        // if (Input.GetButtonDown("Jump") && groundedPlayer)
-        // {
-        //     playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
-        // }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
         controller.Move(playerVelocity * Time.deltaTime);
