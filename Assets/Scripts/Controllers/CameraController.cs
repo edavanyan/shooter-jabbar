86a510bd8237cbdab001945fba8ffcd7f874735a
edgar <edgaravanyan@gmail.com>
2022-09-05 22:42:17 +0400

    camera follow

---
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour, EventListener
+{
+    private PlayerController _playerController;
+    private bool _isInGame = false;//to avoid expensive null check in update 
+
+    [SerializeField] private float _smoothSpeed = 075f;
+    [SerializeField] private Vector3 _offset;
+
+    void Start()
+    {
+        GameManager.Instance.Events.RegisterObserver(this);
+    }
+
+    void LateUpdate()
+    {
+        if (_isInGame)
+        {
+            var destination = _playerController.transform.position + _offset;
+            var smoothPosition = Vector3.Lerp(transform.position, destination, _smoothSpeed);
+            transform.position = smoothPosition;
+        }
+    }
+
+    [EventHandler]
+    void OnPlayerJoined(PlayerJoinedEvent playerJoinedEvent)
+    {
+        _playerController = playerJoinedEvent.Player;
+        _isInGame = true;
+    }
+
+    [EventHandler]
+    void OnPlayerLeft(PlayerLeftEvent playerLeftEvent)
+    {
+        _isInGame = false;
+    }
+}
